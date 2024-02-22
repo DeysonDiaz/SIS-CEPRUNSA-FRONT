6127925daf13f28269a60c2e070e307213ef6b75
DEYSON DIAZ <ddiazti@unsa.edu.pe>
2024-02-22 11:05:01 -0500

    adding styles

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router-dom"
+//import "./Home.css"
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <Outlet />
-        </div>
+        <>
+            <div class="chat-notification">
+                <div class="chat-notification-logo-wrapper">
+                    <img class="chat-notification-logo" src="https://www.rastreator.com/wp-content/themes/rastreator/assets/img/autonomos/autonomos-icon-aseguradoras.svg" alt="ChitChat Logo"/>
+                    <link href="./output.css" rel="stylesheet"></link>
+                </div>
+                <div class="chat-notification-content">
+                    <h4 class="chat-notification-title">ChitChat</h4>
+                    <p class="chat-notification-message">You have a new message!</p>
+                </div>
+            </div>
+            <Outlet /></>
+
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './App.css'
+
 import {
   RouterProvider,
 } from "react-router-dom";
-import { router } from "./Router/Router.jsx";
+import { router } from "./Router/Router"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
